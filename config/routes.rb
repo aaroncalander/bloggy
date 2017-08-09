b1e9203d71a4cebde8bb3e7ff85bf3d0db0e8080
Aaron Calander <aaroncalander@yahoo.com>
2017-08-08 22:36:29 -0700

    Add admin views and styling as well as navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  # devise_for :admins
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   root "posts#index"
